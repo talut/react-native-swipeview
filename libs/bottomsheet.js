603225de9d93ac578cbde142359277a643213fa2
Talut TAŞGIRAN <iletisim@taluttasgiran.com.tr>
2018-05-28 13:50:14 +0300

    children props updated

--- libs/bottomsheet.js
+++ libs/bottomsheet.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Dimensions, ScrollView, View } from 'react-native';
+import { Dimensions, Platform, ScrollView, View } from 'react-native';
 import Interactable from 'react-native-interactable';
 
 const screenSettings = {
@@ -10,7 +10,8 @@ export default class BottomSheet extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      scroll: false
+      scroll        : false,
+      showBackground: false,
     };
     this.onSnap = this.onSnap.bind(this)
   }
@@ -51,32 +52,48 @@ export default class BottomSheet extends React.Component {
       });
     }
   };
+  onStopInteraction = ({nativeEvent}) => {
+    const {y} = nativeEvent
+    if (y === 100) {
+      this.setState({
+        scroll: !this.state.showBackground
+      });
+    } else {
+      this.setState({
+        showBackground: false
+      })
+    }
+  }
 
   render() {
     return (
-      <Interactable.View
-        style={ this.props.style }
-        gravityPoints={ [{x: 0, y: 100, strength: 5000, falloff: 40, damping: 0.5}] }
-        initialPosition={ {y: screenSettings.interactableBottom} }
-        verticalOnly={ true }
-        snapPoints={ [{y: screenSettings.interactableBottom, tension: 1000}, {y: 100}] }
-        boundaries={ {left: 0, right: 0, top: 100, bounce: 0} }
-        onSnap={ this.onSnap }>
-        { this.props.header }
-        <ScrollView
-          onScroll={ this.onScroll }
-          onScrollDragEnd={ this.onScrollDragEnd }
-          scrollEventThrottle={ 100 }
-          scrollEnabled={ this.state.scroll }
-          style={ {
-            maxHeight: Dimensions.get('window').height - 180,
-          } }
-        >
-          <View>
+      <View>
+        <Interactable.View
+          style={ this.props.style }
+          gravityPoints={ [{x: 0, y: 100, strength: 1000, falloff: 40, damping: 0.5}] }
+          initialPosition={ {y: screenSettings.interactableBottom} }
+          verticalOnly={ true }
+          snapPoints={ [{y: screenSettings.interactableBottom}, {y: 50}] }
+          boundaries={ {left: 0, right: 0, top: 100, bounce: 0} }
+          onStop={ this.onStopInteraction }
+          onSnap={ this.onSnap }>
+          { this.props.header }
+          <ScrollView
+            onScroll={ this.onScroll }
+            onScrollDragEnd={ this.onScrollDragEnd }
+            scrollEventThrottle={ 100 }
+            bounces={ Platform.OS === 'ios' ? false : undefined }
+            overScrollMode={ Platform.OS === 'ios' ? undefined : 'never' }
+            scrollEnabled={ true }
+            style={ {
+              height         : Dimensions.get('window').height - 210,
+              backgroundColor: '#FFFFFF',
+            } }
+          >
             { this.props.children }
-          </View>
-        </ScrollView>
-      </Interactable.View>
+          </ScrollView>
+        </Interactable.View>
+      </View>
     );
   }
 }
